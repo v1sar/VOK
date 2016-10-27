d1fb6f5c9e79a1e2fc80d40b1f1a2308ba5ae577
v1sar <v1sar@mail.ru>
2016-10-27 04:01:38 +0300

    added Friends

--- app/src/main/java/com/example/qwerty/vok_test/MainActivity.java
+++ app/src/main/java/com/example/qwerty/vok_test/MainActivity.java
@@ -5,13 +5,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
 import com.vk.sdk.dialogs.VKShareDialog;
 import com.vk.sdk.util.VKUtil;
 
@@ -20,6 +28,8 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +51,24 @@ public class MainActivity extends AppCompatActivity {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
-                Toast.makeText(MainActivity.this, "Good", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                listView = (ListView) findViewById(R.id.list);
+
+                VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name,last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        VKList list = (VKList) response.parsedModel;
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, list);
+                        listView.setAdapter(arrayAdapter);
+                    }
+                });
 // Пользователь успешно авторизовался
             }
             @Override
             public void onError(VKError error) {
-                Toast.makeText(MainActivity.this, "Bad", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
 // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
             }
         })) {
